630b03f6b2908bff49a6c8b444864e11259a8095
Mario De Mario <mariopalmira7@gmail.com>
2026-05-24 19:15:51 +0200

    Estrutura inicial do sistema

---
+++ src/main/java/com/ujcgroupstudents/SellingAndBuyAgricolProdutsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ujcgroupstudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingAndBuyAgricolProdutsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingAndBuyAgricolProdutsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ujcgroupstudents/SellingAndBuyAgricolProdutsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujcgroupstudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellingAndBuyAgricolProdutsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
